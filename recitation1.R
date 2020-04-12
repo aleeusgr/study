9ffe36ed1872ed709119aba5830bee51da16e45a
Alex Seregin <alexeusgr@yahoo.com>
2020-04-12 19:13:57 +0300

    Recitation 1 full

--- recitation1.R
+++ recitation1.R
@@ -9,4 +9,14 @@ high_sodium$Description
 USDA$Sodium[match('CAVIAR',USDA$Description)]
 summary(USDA$Sodium)
 sd(USDA$Sodium, na.rm = TRUE)
-plot(USDA$Protein,USDA$TotalFat)
+plot(USDA$Protein,USDA$TotalFat, xlab="Protein",ylab="Fat",col="red")
+hist(USDA$VitaminC,xlab="Vitamin C, mg", main="Vitamin C",xlim=c(0,100),breaks=1000)
+boxplot(USDA$Sugar,ylab="Sugar, gr")
+USDA$High_Sodium =as.numeric(USDA$Sodium > mean(USDA$Sodium,na.rm = TRUE))
+USDA$High_Fat =as.numeric(USDA$TotalFat > mean(USDA$TotalFat,na.rm = TRUE))
+USDA$High_Protein =as.numeric(USDA$Protein > mean(USDA$Protein,na.rm = TRUE))
+USDA$High_Carbs =as.numeric(USDA$Carbohydrate > mean(USDA$Carbohydrate,na.rm = TRUE))
+table(USDA$High_Sodium,USDA$High_Fat)
+#compute avg of Iron sorted by protein
+tapply(USDA$Iron,USDA$High_Protein,mean, na.rm=TRUE)
+tapply(USDA$VitaminC,USDA$High_Carbs,max, na.rm=TRUE)
